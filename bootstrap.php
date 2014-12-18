568bd3d0dae89c6837e0bdee80ecdce04fce599b
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-18 11:19:35 +0200

    Update version in bootstrap.php to 0.0.5

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Re-Subscribe
  * Description: Modern modal for displaying newsletter subscribtion responsive modal
- * Version: 0.0.1
+ * Version: 0.0.5
  * Author name: Ammar Alakkad
  * Author URI: http://aalakkad.me
  * License: MIT
