05635c8d4979234393e438717a40b35936b0dc99
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-28 14:42:00 +0200

    dashboard: handle warning for undefined variables, add missing form tag

--- src/inc/dashboard_menu.php
+++ src/inc/dashboard_menu.php
@@ -15,29 +15,30 @@ if(isset($_POST)) {
     }
 }
 else {
-    $main_title = get_option('resubscribe-main-title', true);
-    $main_text = get_option('resubscribe-main-text', true);
-    $footer_box_text = get_option('resubscribe-footer-box-text', true);
+    $main_title      = isset($main_title)      ? $main_title      : get_option('resubscribe-main-title', Resubscribe::$main_title);
+    $main_text       = isset($main_text)       ? $main_text       : get_option('resubscribe-main-text', Resubscribe::$main_text);
+    $footer_box_text = isset($footer_box_text) ? $footer_box_text : get_option('resubscribe-footer-box-text', Resubscribe::$footer_box_text);
 }
 
 ?>
 <div class="wrap">
     <h2>Re-Subscribe Options</h2>
+    <form action="">
+        <table class="form-table">
+            <tr>
+                <th><label for="main-text">Modal title</label></th>
+                <td><input type="text" name="main-title" value="<?= $main_title;?>"></td>
+            </tr>
+            <tr>
+                <th><label for="main-text">Main text in the modal</label></th>
+                <td><input type="text" name="main-text" value="<?= $main_text;?>"></td>
+            </tr>
+            <tr>
+                <th><label for="footer-box-text">Footer box text</label></th>
+                <td><input type="text" name="footer-box-text" value="<?= $footer_box_text;?>"></td>
+            </tr>
+        </table>
 
-    <table class="form-table">
-        <tr>
-            <th><label for="main-text">Modal title</label></th>
-            <td><input type="text" name="main-title" value="<?= $main_title;?>"></td>
-        </tr>
-        <tr>
-            <th><label for="main-text">Main text in the modal</label></th>
-            <td><input type="text" name="main-text" value="<?= $main_text;?>"></td>
-        </tr>
-        <tr>
-            <th><label for="footer-box-text">Footer box text</label></th>
-            <td><input type="text" name="footer-box-text" value="<?= $footer_box_text;?>"></td>
-        </tr>
-    </table>
-
-    <input type="submit" class="btn btn-primary" value="Save">
+        <input type="submit" class="button button-primary" value="Save">
+    </form>
 </div>
