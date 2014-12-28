f09180a65df3bb89b4fab9141cab4d29168a1d2f
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-28 12:08:58 +0200

    JS: check for mobile device and either show footer-box or launch the modal

--- assets/resubscribe.js
+++ assets/resubscribe.js
@@ -15,25 +15,29 @@ jQuery(document).ready(function ($) {
 
     var $inst = $.remodal.lookup[$('[data-remodal-id=modal]').data('remodal')];
 
-    // display popup after 20 seconds.
-    setTimeout(function () {
-        $inst.open();
-        setCookie(resubscribe.key, '1', resubscribe.expiration_days);
-    }, 20000);
-
-    // In footer-box, if close link clicked, then hide the footer-box
-    $('#resubscribe-footer-box').find('.close a').click(function() {
-        $('#resubscribe-footer-box').fadeOut();
-        return false;
-    });
-
-    // In footer-box, if subscribe link clicked, show the modal then hide the footer-box
-    $('#resubscribe-footer-box').find('.subscribe a').click(function() {
-        $inst.open();
-        setCookie(resubscribe.key, '1', resubscribe.expiration_days);
-        $('#resubscribe-footer-box').hide();
-        return false;
-    });
+    // Check if mobile device
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('#resubscribe-footer-box').show();
+        // In footer-box, if close link clicked, then hide the footer-box
+        $('#resubscribe-footer-box').find('.close a').click(function() {
+            $('#resubscribe-footer-box').fadeOut();
+            return false;
+        });
+        // In footer-box, if subscribe link clicked, show the modal then hide the footer-box
+        $('#resubscribe-footer-box').find('.subscribe a').click(function() {
+            $inst.open();
+            setCookie(resubscribe.key, '1', resubscribe.expiration_days);
+            $('#resubscribe-footer-box').hide();
+            return false;
+        });
+    }
+    else {
+        // display popup after 20 seconds.
+        setTimeout(function () {
+            $inst.open();
+            setCookie(resubscribe.key, '1', resubscribe.expiration_days);
+        }, 20000);
+    }
 
     $(document).on('confirm', '.remodal', function () {
         // Handle form submittion here (Ajax?)
