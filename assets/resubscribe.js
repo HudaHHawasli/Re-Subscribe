8d47e442358b7c5bb586c56b016dc11dacaa32e2
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-28 10:30:18 +0200

    JS: add code to handle footer-fox links

--- assets/resubscribe.js
+++ assets/resubscribe.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
 
+    var $inst = $.remodal.lookup[$('[data-remodal-id=modal]').data('remodal')];
+
     // If cookie exists don't show the modal
     if(document.cookie.indexOf(resubscribe.key) != -1) {
         return;
@@ -7,9 +9,19 @@ jQuery(document).ready(function ($) {
 
     // display popup after 20 seconds.
     setTimeout(function () {
-        var $inst = $.remodal.lookup[$('[data-remodal-id=modal]').data('remodal')];
         $inst.open();
     }, 20000);
+
+    // In footer-box, if close link clicked, then hide the footer-box
+    $('#resubscribe-footer-box').find('.close a').click(function() {
+        $('#resubscribe-footer-box').fadeOut();
+    });
+
+    // In footer-box, if subscribe link clicked, show the modal then hide the footer-box
+    $('#resubscribe-footer-box').find('.subscribe a').click(function() {
+        $inst.open();
+        $('#resubscribe-footer-box').hide();
+    });
 
     $(document).on('confirm', '.remodal', function () {
         // Handle form submittion here (Ajax?)
