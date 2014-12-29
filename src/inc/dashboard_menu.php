f13a004d7773a27b765ec349749f2107dfac72d1
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-29 12:35:30 +0200

    Dashboard menu: add form to add newsletters, and table to view newsletters

--- src/inc/dashboard_menu.php
+++ src/inc/dashboard_menu.php
@@ -1,5 +1,7 @@
 <?php
 
+$newsletters = get_option('resubscribe-newsletters', []);
+
 if(isset($_POST) and count($_POST)) {
     if(isset($_POST['main-text'])) {
         $main_text = $_POST['main-text'];
@@ -12,6 +14,18 @@ if(isset($_POST) and count($_POST)) {
     if(isset($_POST['footer-box-text'])) {
         $footer_box_text = $_POST['footer-box-text'];
         update_option( 'resubscribe-footer-box-text', $footer_box_text );
+    }
+    // Handle newsletter data
+    if(isset($_POST['newsletter_key']) and isset($_POST['newsletter_key'])) {
+        // check than newsletter key doesn't exist
+        if(! array_key_exists($_POST['newsletter_key'], $newsletters)) {
+            $newsletters[$_POST['newsletters_key']] = [
+                'name' => $_POST['newsletter_name'],
+                'list_id' => $_POST['newsletter_list_id']
+            ];
+
+            update_option('resubscribe-newsletters', $newsletters);
+        }
     }
 }
 
@@ -35,6 +49,48 @@ $footer_box_text = isset($footer_box_text) ? $footer_box_text : get_option('resu
             <tr>
                 <th><label for="footer-box-text">Footer box text</label></th>
                 <td><input type="text" name="footer-box-text" value="<?= $footer_box_text;?>"></td>
+            </tr>
+        </table>
+
+        <input type="submit" class="button button-primary" value="Save">
+    </form>
+
+    <hr>
+
+    <h2>Newsletters</h2>
+
+    <table class="widefat">
+        <thead>
+            <tr>
+                <th>Key</th>
+                <th>Name</th>
+                <th>List_id</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($newsletters as $key => $item): ?>
+            <tr>
+                <td><?= $key;?></td>
+                <td><?= $item['name'];?></td>
+                <td><?= $item['list_id'];?></td>
+            </tr>
+            <?php endforeach;?>
+        </tbody>
+    </table>
+
+    <form action="" method="post">
+        <table class="form-table">
+            <tr>
+                <th><label for="newsletter_name">Name</label></th>
+                <td><input type="text" name="newsletter_name" value="<?= $newsletter_name;?>"></td>
+            </tr>
+            <tr>
+                <th><label for="newsletter_key">Key</label></th>
+                <td><input type="text" name="newsletter_key" value="<?= $newsletter_key;?>"></td>
+            </tr>
+            <tr>
+                <th><label for="newsletter_list_id">MailChimp list id</label></th>
+                <td><input type="text" name="newsletter_list_id" value="<?= $newsletter_list_id;?>"></td>
             </tr>
         </table>
 
